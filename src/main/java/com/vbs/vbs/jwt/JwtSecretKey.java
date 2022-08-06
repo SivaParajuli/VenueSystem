5dcd755af61463a5c44400aa9cd9ec2af6635953
shiva parajuli <parajulisiva9849@gmail.com>
2022-08-06 19:43:06 +0545

    update10

--- src/main/java/com/vbs/vbs/jwt/JwtSecretKey.java
+++ src/main/java/com/vbs/vbs/jwt/JwtSecretKey.java
@@ -1,6 +1,6 @@
 package com.vbs.vbs.jwt;
 
-import com.vbs.vbs.jwt2.JwtConfig;
+import com.vbs.vbs.jwt.JwtConfig;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
