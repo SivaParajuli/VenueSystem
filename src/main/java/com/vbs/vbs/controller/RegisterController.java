944be9aef863b63502344dcba3e3d37f55e51786
shiva parajuli <parajulisiva9849@gmail.com>
2022-08-07 16:39:43 +0545

    maageko login mvn clean update cros

--- src/main/java/com/vbs/vbs/controller/RegisterController.java
+++ src/main/java/com/vbs/vbs/controller/RegisterController.java
@@ -6,13 +6,11 @@ import com.vbs.vbs.dto.VenueDto;
 import com.vbs.vbs.services.RegisterService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path="register")
 public class RegisterController extends BaseController {
