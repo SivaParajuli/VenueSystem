c8d1db92a20a3c9a598f38f8a6aa9cf17c0f7441
shiva parajuli <parajulisiva9849@gmail.com>
2022-08-07 16:52:24 +0545

    maageko login mvn clean update cros2

--- src/main/java/com/vbs/vbs/security/ApplicationSecurityConfig.java
+++ src/main/java/com/vbs/vbs/security/ApplicationSecurityConfig.java
@@ -40,6 +40,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
